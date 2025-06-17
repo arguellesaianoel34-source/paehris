8378fa0022235c590a13c0f0644769642deafcfb
Lucky John Faderon <luckyf@callboxinc.com>
2025-06-17 20:09:44 +0800

    Updated Summary of Cost

--- application/models/model_purchasing.php
+++ application/models/model_purchasing.php
@@ -2258,6 +2258,7 @@ class Model_purchasing extends CI_Model
                                 $supplist['total'] = '<span id="supplier_soc" data-id="' . $supplier->sysid . '">'.number_format($total,2).'</span>';
                             } else {
                                 $gtotal['total'][] = $total_php;
+                                $supplist['php_total'] = '<span id="php_supplier_soc" data-id="' . $supplier->sysid . '">1.00</span>';
                                 $supplist['total'] = '<span id="supplier_soc" data-id="' . $supplier->sysid . '">'.number_format($total_php,2).'</span>';
                             }
                         }
