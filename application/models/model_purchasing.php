9a05721d92acfe503fcea00717c52d89ce1fc671
Lucky John Faderon <lfaderon@gmail.com>
2025-07-18 14:34:50 +0800

    VAT UPDATE 2

--- application/models/model_purchasing.php
+++ application/models/model_purchasing.php
@@ -2215,7 +2215,7 @@ class Model_purchasing extends CI_Model
                     $grosstotal += $gross;
                 } else {
                     $netvat = $supp_total;
-                    $vat = 0;
+                    $vat = 12;
                     $gross = 0;
                     $ewt = 0;
                     $total = $netvat;
