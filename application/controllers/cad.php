df3c5a566b5932e5127905a0a3b17a4b7dfc41b3
Lucky John Faderon <luckyf@callboxinc.com>
2025-07-25 19:31:39 +0800

    updated proposal

--- application/controllers/cad.php
+++ application/controllers/cad.php
@@ -1270,9 +1270,6 @@ class Cad extends CI_Controller
 
         $layout = $this->cad->get_document_layout();
 
-        print_r($layout);
-
-        exit;
 
         $papersize = $layout->papersize;
         $html = $layout->html;
