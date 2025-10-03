45b35ee83985b353cd84b324f76dc23b8b177f80
Lucky John Faderon <lfaderon@gmail.com>
2025-10-03 14:15:07 +0800

    updated layout in deal and add more features.

--- application/config/database.php
+++ application/config/database.php
@@ -90,10 +90,10 @@ $db['audit'] = array_merge($default_db_config, array(
 // ############### PECO MAIN #########################
 // ###################################################
 $db['peco'] = array_merge($default_db_config, array(
-    'hostname' => '44.195.210.193',
-    'username' => 'rwlisxy9bxboh4qh_pecoerp',
-    'password' => 'P3c0!2022web##',
+    'hostname' => '172.174.114.142',
     'database' => 'rwlisxy9bxboh4qh_peco_erp',
+    'username' => 'rwlisxy9bxboh4qh_peco_root',
+    'password' => 'ajLu}^RkQ8#p',
 ));
 
 // ###################################################
